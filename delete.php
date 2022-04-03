2ba493f1ab58abe9d387dfb95ce271f9c01870e7
SergeyBorsh <99021985+SergeyBorsh@users.noreply.github.com>
2022-04-03 20:07:48 +0300

    Delete delete.php

--- delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'db_connect.php';
-$choice = $_POST['choiceDict'];
-$queryDelete = "drop table $choice";
-$startDelete = mysqli_query($dbcon,$queryDelete);
-mysqli_close($dbcon);
