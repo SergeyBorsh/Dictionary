c6c57fe2f36d8e02cdf0a57a9f2b0c7986de48c9
SergeyBorsh <99021985+SergeyBorsh@users.noreply.github.com>
2022-04-03 20:07:36 +0300

    Delete db_connect.php

--- db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$root='root';
-$pass='';
-$host='localhost';
-$db='dict1';
-$dbcon=mysqli_connect($host,$root,$pass,$db); 
-if (!$dbcon) {
-    die("Connection failed: " . mysqli_connect_error());
-}
